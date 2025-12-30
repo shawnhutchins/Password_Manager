2e5b9daabc4804eb5d1ca668cbb73a11154565d6
shawn <shawnhutchins1@gmail.com>
2025-12-29 21:45:22 -0500

    Added window canvas with the logo image

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.configure( padx=20, pady=20)
+
+canvas = Canvas(master=window, width=200, height=200)
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(row=0, column=0)
+
+window.mainloop()
