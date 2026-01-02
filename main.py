b268610b1d48d961e32f88f537474d3df2709e37
Shawn <shawnhutchins1@gmail.com>
2026-01-01 23:24:24 -0500

    Added Labels and comments for the entries

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ window = Tk()
 window.title("Password Manager")
 window.configure( padx=20, pady=20)
 
+#Logo
 canvas = Canvas(master=window, width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
-canvas.grid(row=0, column=0)
+
+#Labels
+website_label = Label(master=window, text="Website:")
+username_label = Label(master=window, text="Email/Username:")
+password_label = Label(master=window, text="Password:")
+
+#Grid ---------------------------------------------------
+canvas.grid(row=0, column=1)
 
 window.mainloop()
